4ce349c481936248015e5f22c56136d6d20b0bae
Burton Smith <bsmith@amazazing.com>
2019-09-25 11:13:16 -0400

    Add lazy-loading images.

---
+++ src/components/image/image.stories.js
@@ -0,0 +1,23 @@
+import { text, select, boolean, number } from "@storybook/addon-knobs";
+
+export default { title: 'Images' };
+
+export const lazyImage = () => { 
+    const src = text('src', 'https://imgix.bustle.com/rehost/2016/9/13/ffa8cec2-85d9-40ad-b386-906b75bf5610.jpg?w=970&h=582&fit=crop&crop=faces&auto=format&q=70');
+    const src2 = text('src2', 'https://i.pinimg.com/originals/84/dd/ee/84ddee6dff0e2af20754826aecbc5c11.jpg');
+    const alt = text('alt', 'Days of no nonsense');
+    const lazy = boolean('lazy', true);
+    const threshold = number('threshold', 300);
+    return(`<div style="height:100vh;margin-bottom:500px;">
+                <h1>Scroll Down</h1>
+            </div>
+            <div>
+                <c-img alt="${alt}" lazy="${lazy}" src="${src}" threshold="${threshold}" />
+            </div>
+            <div style="height:100vh;margin-bottom:500px;">
+                <h2>Scroll Down Some More</h2>
+            </div>
+            <div>
+                <c-img alt="${alt}" lazy="${lazy}" src="${src2}" threshold="${threshold}" />
+            </div>`);
+};
---
+++ src/components/image/readme.md
@@ -0,0 +1,22 @@
+# c-img
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property    | Attribute   | Description | Type      | Default     |
+| ----------- | ----------- | ----------- | --------- | ----------- |
+| `alt`       | `alt`       |             | `string`  | `undefined` |
+| `cssClass`  | `css-class` |             | `string`  | `undefined` |
+| `htmlId`    | `html-id`   |             | `string`  | `undefined` |
+| `lazy`      | `lazy`      |             | `boolean` | `undefined` |
+| `src`       | `src`       |             | `string`  | `undefined` |
+| `threshold` | `threshold` |             | `number`  | `300`       |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
