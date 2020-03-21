b8b5f8f737cd5688c0776f881806247d2a6c321c
break-stuff <burton@breakstuff.com>
2020-03-21 19:02:45 -0400

    Removed invalid story

--- src/scripts/components/overlay/overlay.stories.js
+++ src/scripts/components/overlay/overlay.stories.js
@@ -2,20 +2,7 @@ import { text, select, boolean, number } from "@storybook/addon-knobs";
 
 export default { title: 'Overlay' };
 
-export const basicOverlay = () => {
-    const show = boolean('show', false);
-    const absolute = boolean('absolute', false);
-    const theme = select('theme', ['dark', 'light'], 'dark');
-    const container = document.createElement('div');
-    const overlay = document.createElement('c-overlay');
-    container.className = 'w-33';
-    overlay.absolute = absolute;
-    overlay.theme = theme;
-    container.innerHTML = `<c-overlay show="${show}" absolute="${absolute}" theme="${theme}">Hello!!!</c-overlay>`;
-    return container;
-};
-
-export const test = () => {
+export const playground = () => {
     const theme = select('theme', ['dark', 'light'], 'dark');
     const absolute = boolean('absolute', false);
     const requireResponse = boolean('require-response', false);
