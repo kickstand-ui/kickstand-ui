46bf2f1cc6511fae63fa99c07233823e47ddcddc
break-stuff <burton@breakstuff.com>
2020-03-25 11:42:29 -0400

    fix drop-down keyboard controls

--- src/scripts/components/dropdown/dropdown.tsx
+++ src/scripts/components/dropdown/dropdown.tsx
@@ -5,7 +5,10 @@ import componentUtils from '../../utils/componentUtils';
     tag: 'c-dropdown'
 })
 export class Dropdown {
-    private dropdownId = componentUtils.uniqueId();
+    dropdownId = componentUtils.uniqueId();
+    $contents: HTMLElement;
+    $control: HTMLElement;
+    $focusableEls: HTMLElement[];
 
     @Element() $el: HTMLElement;
 
@@ -13,48 +16,81 @@ export class Dropdown {
     @Prop() dropdownAlignment: string;
     @Prop() buttonIcon: string;
     @Prop() buttonType: 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark' | 'link' = 'primary';
-    @Prop() hollow: boolean;
+    @Prop() hollowButton: boolean;
+    @Prop() clearButton: boolean;
     @Prop() hideButtonText: boolean;
     @Prop() megaMenu: boolean;
 
-    @State() expanded: boolean = false;
+    @State() isExpanded: boolean = false;
+    @State() focusIndex: number = 0;
+
+    @Listen('keydown')
+    handleKeyDown(e: KeyboardEvent) {
+        const KEY_TAB = 9;
+        const KEY_ESC = 27;
+
+        switch (e.keyCode) {
+            case KEY_TAB:
+                this.handleTab(e);
+                break;
+            case KEY_ESC:
+                this.handleEsc();
+                break;
+            default:
+                break;
+        }
+    }
+
+    handleEsc() {
+        this.close();
+    }
+
+    handleTab(e: KeyboardEvent) {
+        e.preventDefault();
+
+        if (e.shiftKey) {
+            this.handleBackwardTab();
+        } else {
+            this.handleForwardTab();
+        }
+    }
+
+    handleBackwardTab() {
+        this.focusIndex = this.focusIndex === 0 ? this.$focusableEls.length - 1 : --this.focusIndex;
+        this.$focusableEls[this.focusIndex].focus();
+    }
+
+    handleForwardTab() {
+        this.focusIndex = this.$focusableEls.length - 1 === this.focusIndex ? 0 : ++this.focusIndex;
+        this.$focusableEls[this.focusIndex].focus();
+    }
 
 
-    constructor() {
+    componentDidRender() {
+        this.$focusableEls = Array.from(this.$contents.querySelectorAll(componentUtils.focusableElements));
+
         window.addEventListener('click', (e: any) => {
             let isChildElement = this.$el.contains(e.target)
 
             if (isChildElement)
                 return;
 
-            this.expanded = false;
+            this.isExpanded = false;
         });
     }
 
-    @Listen('closeDropdown')
-    closeDropdownHandler() {
-        this.expanded = false;
+    toggleDropdown() {
+        this.isExpanded = !this.isExpanded;
+
+        if (this.isExpanded && this.$focusableEls.length > 0)
+            setTimeout(() => this.$focusableEls[0].focus(), 100);
     }
 
-    private keyupHandler(e) {
-        if (e.keyCode === 27) {
-            this.expanded = false;
-            this.$el.querySelector('button').focus();
-        }
-    }
-
-    private toggleDropdown() {
-        this.expanded = !this.expanded;
-
-        if (this.expanded)
-            this.setDropdownFocus();
-    }
-
-    private setDropdownFocus() {
-        setTimeout(() => {
-            const dropdownContents: any = this.$el.querySelector('.dropdown-contents') || this.$el.querySelector('.mega-menu-contents');
-            dropdownContents.focus();
-        });
+    close() {
+        this.isExpanded = false;
+        this.focusIndex = 0;
+        this.$control.setAttribute("aria-expanded", "false");
+        setTimeout(() => this.$control.focus());
     }
 
     render() {
@@ -62,17 +98,16 @@ export class Dropdown {
             <Host class={{
                 'dropdown': true,
                 'mega-menu': this.megaMenu
-            }}
-                onKeyup={this.keyupHandler.bind(this)}>
-                <button onClick={this.toggleDropdown.bind(this)} class={`button ${this.buttonType} ${this.hollow && 'hollow'} dropdown-button`} aria-haspopup="true" aria-expanded={`${this.expanded}`} aria-controls={`dropdown-${this.dropdownId}`}>
+            }}>
+                <c-button onClick={this.toggleDropdown.bind(this)} type={this.buttonType} hollow={this.hollowButton} clear={this.clearButton} class={`button ${this.buttonType} ${this.hollowButton && 'hollow'} dropdown-button`} aria-haspopup="true" aria-expanded={`${this.isExpanded}`} aria-controls={`dropdown-${this.dropdownId}`} ref={el => this.$control = el}>
                     {this.buttonIcon && <i class={`button-icon ${this.buttonIcon}`}></i>}
                     <span class={{
                         'dropdown-text': true,
                         'sr-only': this.hideButtonText
                     }}>{this.dropdownText}</span>
                     <i class="fas fa-chevron-down dropdown-icon"></i>
-                </button>
-                <div id={`dropdown-${this.dropdownId}`} class={this.megaMenu ? 'mega-menu-contents' : `dropdown-contents align-${this.dropdownAlignment}`} role={this.megaMenu ? '' : 'list'} tabindex="-1">
+                </c-button>
+                <div id={`dropdown-${this.dropdownId}`} class={this.megaMenu ? 'mega-menu-contents' : `dropdown-contents align-${this.dropdownAlignment}`} role={this.megaMenu ? '' : 'list'} tabindex="-1" ref={el => this.$contents = el}>
                     <slot />
                 </div>
             </Host>
