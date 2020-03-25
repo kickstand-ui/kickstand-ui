d34f7e5da8fb3efc87a84caad27d11b5bcb9edd8
break-stuff <burton@breakstuff.com>
2020-03-25 11:43:44 -0400

    Fix side-drawer implementation

--- src/scripts/components/side-drawer/readme.md
+++ src/scripts/components/side-drawer/readme.md
@@ -7,17 +7,50 @@
 
 ## Properties
 
-| Property        | Attribute        | Description | Type       | Default     |
-| --------------- | ---------------- | ----------- | ---------- | ----------- |
-| `clickHandler`  | --               |             | `Function` | `undefined` |
-| `cssClass`      | `css-class`      |             | `string`   | `''`        |
-| `hollow`        | `hollow`         |             | `boolean`  | `undefined` |
-| `href`          | `href`           |             | `string`   | `undefined` |
-| `icon`          | `icon`           |             | `string`   | `undefined` |
-| `iconDirection` | `icon-direction` |             | `string`   | `'left'`    |
-| `size`          | `size`           |             | `string`   | `undefined` |
-| `type`          | `type`           |             | `string`   | `'primary'` |
+| Property     | Attribute     | Description | Type                | Default     |
+| ------------ | ------------- | ----------- | ------------------- | ----------- |
+| `headerText` | `header-text` |             | `string`            | `undefined` |
+| `name`       | `name`        |             | `string`            | `undefined` |
+| `position`   | `position`    |             | `"left" \| "right"` | `'left'`    |
 
+
+## Methods
+
+### `hide() => Promise<void>`
+
+
+
+#### Returns
+
+Type: `Promise<void>`
+
+
+
+### `show() => Promise<void>`
+
+
+
+#### Returns
+
+Type: `Promise<void>`
+
+
+
+
+## Dependencies
+
+### Depends on
+
+- [c-overlay](../overlay)
+- [c-button](../button)
+
+### Graph
+```mermaid
+graph TD;
+  c-side-drawer --> c-overlay
+  c-side-drawer --> c-button
+  style c-side-drawer fill:#f9f,stroke:#333,stroke-width:4px
+```
 
 ----------------------------------------------
 
--- src/scripts/components/side-drawer/side-drawer.tsx
+++ src/scripts/components/side-drawer/side-drawer.tsx
@@ -4,82 +4,57 @@ import { Component, h, Prop, Element, EventEmitter, Event, ComponentInterface, M
     tag: 'c-side-drawer'
 })
 export class SideDrawer implements ComponentInterface {
+    $overlay: HTMLCOverlayElement;
+    $drawer: HTMLElement;
+
     @Element() $el: HTMLElement;
 
-    @Prop() position: string = 'left';
+    @Prop() position: 'left' | 'right' = 'left';
     @Prop() name: string;
     @Prop() headerText: string;
-    @Prop() onOpen: Function;
-    @Prop() onClose: Function;
 
-    @State() closeEventName: string = `close-side-drawer:${this.name}`;
-    @State() openEventName: string = `open-side-drawer:${this.name}`;
+    @State() isShowing: boolean;
 
-    @Event({ eventName: this.closeEventName, bubbles: true }) closeDrawerCompleted: EventEmitter;
-    @Event({ eventName: this.openEventName, bubbles: true }) openDrawerCompleted: EventEmitter;
-
-    clickHandler(e: MouseEvent) {
-        let isChildElement = this.$el.querySelector('.contents').contains(e.target as Element);
-
-        if (isChildElement)
-            return;
-
-        this.closeDrawer();
-    }
-
-    keyUpHandler(e: KeyboardEvent) {
-        if (e.keyCode === 27) {
-            this.closeDrawer();
-        }
-    }
-
-    closeDrawer() {
-        this.$el.classList.remove('open');
-        this.onClose();
-
-        let $control = document.querySelector<HTMLElement>(`[aria-controls="${this.name}"]`);
-
-        if ($control) {
-            $control.setAttribute("aria-expanded", "false");
-            $control.focus();
-        }
-
-        this.closeDrawerCompleted.emit();
+    @Method()
+    async hide() {
+        this.isShowing = false;
+        await this.$overlay.hide()
     }
 
     @Method()
-    open() {
-        this.$el.classList.add('open');
-        this.openDrawerCompleted.emit();
-        let $control = document.querySelector(`[aria-controls="${this.name}"]`);
-
-        if ($control)
-            $control.setAttribute("aria-expanded", "true");
-
-        this.$el.querySelector<HTMLElement>('.drawer-close').focus();
+    async show() {
+        await this.$overlay.show();
+        this.isShowing = true;
     }
 
-    @Method()
-    close() {
-        this.closeDrawer();
+    componentDidRender() {
+        const $scrim = this.$overlay.querySelector('.scrim');
+        $scrim.addEventListener('click', () => {
+            this.hide();
+        });
     }
 
     render() {
+        let classes = {
+            'side-drawer': true,
+            [this.position]: true,
+            'show': this.isShowing
+        }
         return (
-            <Host id={this.name} class="side-drawer" onClick={this.clickHandler.bind(this)} onKeyup={this.keyUpHandler.bind(this)}>
-                <aside class={`contents ${this.position}`} tabindex="-1">
-                    <header class="side-drawer-header">
-                        <h3>{this.headerText}</h3>
-                        <button class="button drawer-close" onClick={this.closeDrawer.bind(this)}>
-                            <span class="sr-only">close menu</span>
+            <c-overlay ref={el => this.$overlay = el}>
+                <aside class={classes} ref={el => this.$drawer = el}>
+                    <header class="drawer-header">
+                        <h3 class="drawer-title">{this.headerText}</h3>
+                        <c-button onClick={() => this.hide()} class="drawer-close" clear>
+                            <span class="sr-only">Close Drawer</span>
                             <i class="fal fa-times"></i>
-                        </button>
+                        </c-button>
                     </header>
-                    <div class="side-drawer-body">
+                    <div class="drawer-body">
                         <slot />
                     </div>
                 </aside>
-            </Host>
+            </c-overlay>
         );
     }
 }
