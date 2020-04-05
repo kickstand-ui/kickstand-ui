bf9d9246b601a507198b5bcb4807bb2ebbfb70bc
break-stuff <burton@breakstuff.com>
2020-04-05 16:31:56 -0400

    fix grid stories

--- src/scripts/components/grid/grid.stories.js
+++ src/scripts/components/grid/grid.stories.js
@@ -7,19 +7,19 @@ export const basicGrid = () => {
     <div class="main">
         <h2>Grid w/ Sub-grid</h2>
         <c-row class="bg-primary">
-            <c-column  align="end" class="col-2 md:col-6 sm:col-12 bg-primary-light">Column 1</div>
+            <c-column class="col-2 md:col-6 sm:col-12 bg-primary-light">Column 1</c-column>
             <c-column class="col-4 md:col-6 sm:col-12 bg-primary-light">Column 2</c-column>
             <c-column class="col-6 md:col-12 sm:col-12 bg-primary-light">
-                <c-row>
+                <div class="row">
                     <c-column>Sub-column 1</c-column>
                     <c-column>Sub-column 2</c-column>
                     <c-column>Sub-column 3</c-column>
-                </c-row>
+                </div>
             </c-column>
         </c-row>
         <br>
         <h2>Grid w/ No Gutters and Sub-grid</h2>
-        <c-row align="end" class="bg-primary" no-gutter>
+        <c-row class="bg-primary" no-gutter>
             <c-column class="col-2 md:col-6 sm:col-12 bg-primary-light">Column 1</c-column>
             <c-column class="col-4 md:col-6 sm:col-12 bg-primary-light">Column 2</c-column>
             <c-column class="col-6 md:col-12 sm:col-12 bg-primary-light">
@@ -32,7 +32,7 @@ export const basicGrid = () => {
         </c-row>
         <br>
         <h2>Grid Using Flex-Layout Classes</h2>
-        <c-row no-gutter class="bg-primary column">
+        <c-row class="bg-primary column">
             <c-column class="col-4 md:col-6 sm:col-12 bg-primary-light">Column 1</c-column>
             <c-column class="col-4 md:col-6 sm:col-12 bg-primary-light">Column 2</c-column>
             <c-column class="col-4 md:col-6 sm:col-12 bg-primary-light">Column 3</c-column>
