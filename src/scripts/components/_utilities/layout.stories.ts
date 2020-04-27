b74a551b60449e4410836d83a886ef3a60c766b4
break-stuff <burton@breakstuff.com>
2020-04-27 08:56:41 -0400

    add height utility

--- src/scripts/components/_utilities/layout.stories.ts
+++ src/scripts/components/_utilities/layout.stories.ts
@@ -1,9 +1,16 @@
 export default { title: 'Utilities/Layouts' };
 
-const widths = [10, 20, 25, 30, 33, 40, 50, 60, 66, 70, 75, 80, 90, 100];
+const widthSizes = [10, 20, 25, 30, 33, 40, 50, 60, 66, 70, 75, 80, 90, 100];
+const heightSizes = [25, 50, 75, 100];
 
-export const sizes = () => {
+export const widths = () => {
     return (`<div class="m-xxl">
-        ${widths.map(s => `<div class="my-md p-sm bg-light w-${s}">w-${s}</div>`).join('')}
+        ${widthSizes.map(s => `<div class="my-md p-sm bg-light w-${s}">w-${s}</div>`).join('')}
     </div>`);
+};
+
+export const heights = () => {
+    return (`<c-row class="m-xxl bg-light" style="height: 300px;">
+        ${heightSizes.map(s => `<c-column class="mx-md p-sm bg-primary-light h-${s}">h-${s}</c-column>`).join('')}
+    </c-row>`);
 };
