4c6b3874e8803e9f438cb24df3dd2c9679901132
break-stuff <burton@breakstuff.com>
2020-04-27 08:58:01 -0400

    updated content

--- src/scripts/components/tabs/tabs.stories.ts
+++ src/scripts/components/tabs/tabs.stories.ts
@@ -5,20 +5,24 @@ export default { title: 'Containers/Tabs' };
 export const defaultTabs = () => {
     const position = select('position', ['top', 'bottom', 'left', 'right'], 'top');
     return (`                            
-        <c-tabs class="m-xxl" position="${position}" name="categories">
+        <c-tabs class="m-xxl" position="${position}" name="categories" style="max-width: 900px;">
             <c-tab-list>
-                <c-tab controls="test-1">Test 1</c-tab>
-                <c-tab controls="test-2">Test 2</c-tab>
-                <c-tab controls="test-3">Test 3</c-tab>
+                <c-tab controls="test-1">Three Hole Punch Jim</c-tab>
+                <c-tab controls="test-1">Dave</c-tab>
+                <c-tab controls="test-2">Facebook</c-tab>
+                <c-tab controls="test-3">Popeye</c-tab>
             </c-tab-list>
             <c-tab-panel tab-id="test-1">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, accusamus officia iure amet in ex numquam eligendi cupiditate labore. Sapiente cumque tenetur enim quos repudiandae suscipit officia, id maiores. Repellat.
+                <img src="https://badgerherald.com/media/2014/10/Halloween-Jim_Three-Hole-Punch-Jim.jpg" alt="jim" />
+            </c-tab-panel>
+            <c-tab-panel tab-id="test-1">
+                <img src="https://img.buzzfeed.com/buzzfeed-static/static/2018-01/10/12/asset/buzzfeed-prod-fastlane-03/sub-buzz-20247-1515605252-1.jpg" alt="jim" />
             </c-tab-panel>
             <c-tab-panel tab-id="test-2">
-                Ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, accusamus officia iure amet in ex numquam eligendi cupiditate labore. Sapiente cumque tenetur enim quos repudiandae suscipit officia, id maiores. Repellat.
+                <img src="https://s-i.huffpost.com/gen/2137796/images/o-JOHN-KRASINSKI-FACEBOOK-facebook.jpg" alt="jim" />
             </c-tab-panel>
             <c-tab-panel tab-id="test-3">
-                Dolor, sit amet consectetur adipisicing elit. Distinctio, accusamus officia iure amet in ex numquam eligendi cupiditate labore. Sapiente cumque tenetur enim quos repudiandae suscipit officia, id maiores. Repellat.
+                <img src="https://th.bing.com/th/id/OIP.phya83cWh3fCeJ1ExctZIQHaEK?pid=Api&rs=1" alt="jim" />
             </c-tab-panel>
         </c-tabs>
     `);
