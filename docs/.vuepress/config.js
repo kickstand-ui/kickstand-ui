4e3e1c308e554093760a6fb35df6a484ff5fa8ff
break-stuff <31320098+break-stuff@users.noreply.github.com>
2020-05-21 21:49:15 -0400

    Add Kickstand to the Docs (#4)
    * Add initial documentation structure
    
    * add deployment script for docs
    
    * Add deployment script
    
    * Add kickstand to the project
    
    Co-authored-by: Burton Smith <bsmith@amazazing.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,10 @@ module.exports = {
     description:
         "A customizable design systems and component library built with native web components",
     head: [
-        ["link", { rel: "icon", href: "/images/kickstand_icon.png" }]
+        ["link", { rel: "icon", href: "/images/kickstand_icon.png" }],
+        ["link", { rel: "stylesheet", href: "https://unpkg.com/kickstand-ui/dist/kickstand-ui/kickstand-ui.css" }],
+        ["script", { type: "module", src: "https://unpkg.com/kickstand-ui/dist/kickstand-ui/kickstand-ui.js" }],
+        ["script", { nomodule: true, href: "https://unpkg.com/kickstand-ui/dist/kickstand-ui/kickstand-ui.js" }],
     ],
     themeConfig: {
         logo: "/images/kickstand_logo.png",
