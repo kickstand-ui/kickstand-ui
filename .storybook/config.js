43cf0655caac9de4d846a8e895067d01a92fe333
Burton Smith <31320098+break-stuff@users.noreply.github.com>
2020-06-26 07:05:38 -0400

    updated how elements were added in stroybook (#48)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
-import '../src/scss/styles.scss';
-import { addParameters, configure, addDecorator } from '@storybook/html';
-import { withKnobs } from '@storybook/addon-knobs';
-import { withA11y } from '@storybook/addon-a11y';
+import "../src/scss/styles.scss";
+import { applyPolyfills } from "../dist/esm/polyfills";
+import { defineCustomElements } from "../dist/esm/loader";
 
-import kickstandUiTheme from './kickstandUiTheme';
+applyPolyfills().then(() => {
+    defineCustomElements();
+});
+
+import { addParameters, configure, addDecorator } from "@storybook/html";
+import { withKnobs } from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
+
+import kickstandUiTheme from "./kickstandUiTheme";
+
 
 addParameters({
     options: {
@@ -14,4 +22,7 @@ addParameters({
 addDecorator(withKnobs);
 addDecorator(withA11y);
 
-configure(require.context('../src/scripts/components', true, /\.stories\.ts$/), module);
+configure(
+    require.context("../src/scripts/components", true, /\.stories\.ts$/),
+    module
+);
