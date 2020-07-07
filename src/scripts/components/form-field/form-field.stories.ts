3e892649d7c8b88391f0b3b57dd18b33bd45ac05
Burton Smith <31320098+break-stuff@users.noreply.github.com>
2020-07-06 20:56:05 -0400

    add height to form field (#80)

--- src/scripts/components/form-field/form-field.stories.ts
+++ src/scripts/components/form-field/form-field.stories.ts
@@ -2,7 +2,7 @@ import { text, select, boolean, number } from '@storybook/addon-knobs';
 
 export default { title: 'Form Field' };
 
-const types = ['text', 'tel', 'url', 'password', 'date', 'email', 'search', 'number', 'hidden', 'spin-box', 'textarea', 'select', 'datalist'];
+const types = ['text', 'tel', 'url', 'password', 'date', 'email', 'search', 'number', 'hidden', 'color', 'file', 'month', 'range', 'spin-box', 'textarea', 'select', 'datalist'];
 
 export const playground = () => {
     const type = select('type', types, 'text');
