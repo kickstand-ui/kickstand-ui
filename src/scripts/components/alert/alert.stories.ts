1b82f734c8a8ecbdb3d01976648afddd2c183a68
Burton Smith <burton@breakstuff.io>
2020-07-12 21:41:03 -0400

    add alert background color

--- src/scripts/components/alert/alert.stories.ts
+++ src/scripts/components/alert/alert.stories.ts
@@ -3,7 +3,7 @@ import { text, select, boolean } from "@storybook/addon-knobs";
 export default { title: 'Alert' };
 
 const colors = ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark'];
-const dummyText = `“People will never be replaced by machines. In the end, life and business are about human connections. And computers are about trying to murder you in a lake. And to me the choice is easy.”<br><br>Michael Scott`;
+const dummyText = `“People will never be replaced by machines. In the end, life and business are about human connections. And computers are about trying to murder you in a lake. And to me the choice is easy.”<br><br>-- Michael Scott`;
 export const defaultHeading = () => {
     const color = select('color', colors, 'primary');
     const header = text('header', 'The Facts of Life by Michael Scott');
