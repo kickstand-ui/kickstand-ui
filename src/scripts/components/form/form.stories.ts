0129a8e1854e8baf502c4918bab61ab499d90efa
Burton Smith <31320098+break-stuff@users.noreply.github.com>
2020-07-25 23:50:54 -0400

    add custom select styling (#99)

--- src/scripts/components/form/form.stories.ts
+++ src/scripts/components/form/form.stories.ts
@@ -20,6 +20,12 @@ export const form = () => {
         <ks-form-field label="Name" required></ks-form-field>
         <ks-form-field label="Email" type="email" value="bad.email"></ks-form-field>
         <ks-form-field label="Accept Terms and Conditions" type="checkbox" required></ks-form-field>
+        <ks-form-field label="Select Test" type="select">
+            <option selected>Choose a value</option>
+            <option>Value 1</option>
+            <option>Value 2</option>
+            <option>Value 3</option>
+        </ks-form-field>
         <ks-button type="submit">Submit</ks-button>
     </ks-form>`);
 };
