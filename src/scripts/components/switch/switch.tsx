aa6142d4ed0c038ae34bec4ae2716d19c021c45c
break-stuff <burton@breakstuff.com>
2020-08-28 17:10:49 -0400

    fix switch button type

--- src/scripts/components/switch/switch.tsx
+++ src/scripts/components/switch/switch.tsx
@@ -30,7 +30,7 @@ export class Switch implements ComponentInterface {
     render() {
         return (
             <Host class="ks-switch">
-                <button id={this.switchId} class="control" role="switch" aria-checked={`${this.checked}`} aria-labelledby={`${this.switchId}_label`} onClick={() => this.clickHandler()} disabled={this.disable}>
+                <button type="button" id={this.switchId} class="control" role="switch" aria-checked={`${this.checked}`} aria-labelledby={`${this.switchId}_label`} onClick={() => this.clickHandler()} disabled={this.disable}>
                     <span class="track"></span>
                     <span class="toggle"></span>
                 </button>
