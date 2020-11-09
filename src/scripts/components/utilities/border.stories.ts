3495dc79dfd9cd79578a3dcec9dbaa54ac66bc1c
Burton Smith <burton@breakstuff.io>
2020-11-08 20:20:00 -0500

    update border utility classes

--- src/scripts/components/utilities/border.stories.ts
+++ src/scripts/components/utilities/border.stories.ts
@@ -5,19 +5,27 @@ const colors = ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 
 const sizes = ['xxxs', 'xxs', 'xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl'];
 
 export const borders = () => {
-    return (`<ks-row class="m-xxl">
-        ${positions.map(p => `<ks-column class="mx-md bg-light-light border${p}">border${p}</ks-column>`).join('')}
-    </ks-row>`);
+    return (`${ sizes.map(s => `<ks-row class="m-xxl">
+        ${positions.map(p => `<ks-column class="mx-md bg-light-light b${p}-${s}">b${p}-${s}</ks-column>`).join('')}
+    </ks-row>`).join('')}`);
 };
 
 export const borderSizes = () => {
     return (`<ks-row class="m-xxl">
-        ${sizes.map(p => `<ks-column class="mx-md bg-light-light border border-${p}">border-${p}</ks-column>`).join('')}
+        ${sizes.map(p => `<ks-column class="mx-md bg-light-light b-xs b-${p}">border-${p}</ks-column>`).join('')}
     </ks-row>`);
 };
 
 export const borderColors = () => {
-    return (`<ks-row class="m-xxl">
-        ${colors.map(p => `<ks-column class="mx-md bg-light-light border border-xs border-${p}">border-${p}</ks-column>`).join('')}
-    </ks-row>`);
+    return (`${ colors.map(c => `
+    <h3>${c}</h3>
+    <ks-row class="my-xl">
+        ${positions.map(p => `<ks-column class="mr-md ml-xs bg-light-light b-xxs b${p}-${c}">b${p}-${c}</ks-column>`).join('')}
+    </ks-row>`).join('')}`);
+};
+
+export const borderRadiuses = () => {
+    return (`${ ['none', 'sm', 'md', 'lg', 'xl', 'pill', 'circle'].map(x => `<ks-row class="m-xxl">
+        ${['', '-t', '-tl', '-tr', '-b', '-bl', '-br', '-l', '-r'].map(p => `<ks-column class="mx-md bg-light-light p-md b-xs br${p}-${x}">br${p}-${x}</ks-column>`).join('')}
+    </ks-row>`).join('')}`);
 };
