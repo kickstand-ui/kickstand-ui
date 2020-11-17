d2ac7f90ca4a9529403ea1db6520959d642665eb
Burton Smith <burton@breakstuff.io>
2020-11-17 06:44:39 -0500

    fix icon rendering for IE 11

--- src/scripts/components/icon/icon.stories.ts
+++ src/scripts/components/icon/icon.stories.ts
@@ -6,8 +6,10 @@ export default { title: 'Components/Icons' };
 export const icons = () => {
     const icon = text('icon', 'activity');
     const rotate = select('rotate', [0, 45, 90, 135, 180, 225, 270, 360, -45, -90, -135, -180, -225, -270, -360], 0);
+    const label = text('label', '');
+    const labelledBy = text('labelled-by', '');
     const cssClass = text('class', '');
-    return (`<p>This is some text! <ks-icon icon="${icon}" rotate="${rotate}" class="${cssClass}" label="test" /></p>`);
+    return (`<p>This is some text! <ks-icon icon="${icon}" rotate="${rotate}" label="${label}" labelled-by="${labelledBy}" class="${cssClass}" label="test" /></p>`);
 };
 
 export const listOfIcons = () => {
--- src/scripts/components/icon/icon.tsx
+++ src/scripts/components/icon/icon.tsx
@@ -1,34 +1,36 @@
-import { Component, h, Host, Prop, ComponentInterface } from '@stencil/core';
+import { Component, h, Host, Prop, ComponentInterface, State, Element } from '@stencil/core';
 import { iconList } from './icon-list';
 
 @Component({
     tag: 'ks-icon',
     styleUrl: 'icon.scss'
 })
-export class Icons implements ComponentInterface {
+export class Icon implements ComponentInterface {
     svgBase: SVGElement;
+    @Element() $el: HTMLElement;
+
     @Prop() label: string;
     @Prop() labelledBy: string;
     @Prop() icon: string;
     @Prop() rotate: '0' | '45' | '90' | '135' | '180' | '225' | '270' | '360' | '-45' | '-90' | '-135' | '-180' | '-225' | '-270' | '-360' = '0';
 
+    @State() iconContent: Document;
+
     componentDidRender() {
-        let icon: string = iconList.find(x => x.name === this.icon)?.icon 
+        this.getIcon();
+    }
+
+    private getIcon() {
+        let iconContents: string = iconList.find(x => x.name === this.icon)?.icon 
             || iconList.find(x => x.name === 'question').icon;
-        this.svgBase.innerHTML = icon;
+            
+        let icon = `<svg class="icon" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" ${this.labelledBy ? `aria-labelledby="${this.labelledBy}"`: ''} ${this.label ? `aria-label="${this.label}"`: ''} ${!this.label && !this.labelledBy ? 'aria-hidden="true"': ''}>${iconContents}</svg>`
+        this.$el.innerHTML = icon;
     }
 
     render() {
-        let props = {
-            ['aria-labelledby']: this.labelledBy,
-            ['aria-label']: this.label,
-            ['aria-hidden']: (!this.label && !this.labelledBy) && 'true'
-        };
-
         return (
-            <Host class={`rotate-${this.rotate}`}>
-                <svg class="icon" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props} ref={e => this.svgBase = e}></svg>
-            </Host>
+            <Host class={`rotate-${this.rotate}`}></Host>
         );
     }
 }
