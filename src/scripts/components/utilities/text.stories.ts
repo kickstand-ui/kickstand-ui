a695d697519225fcd6b55008010706a89f6b66f9
Burton Smith <burton@breakstuff.io>
2021-01-15 07:11:25 -0500

    update default heading styles

--- src/scripts/components/utilities/text.stories.ts
+++ src/scripts/components/utilities/text.stories.ts
@@ -6,9 +6,20 @@ const align = ['left', 'center', 'right'];
 
 export const sizes = () => {
     return (`<div class="m-xxl">
-        ${textSizes.map(s => `<div class="text-${s}">text-${s}</div>`).join('')}
+        ${textSizes.reverse().map(s => `<div class="text-${s}">text-${s}</div>`).join('')}
     </div>`);
 };
+
+export const headings = () => {
+    return (`<div class="m-xxl">
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
+    </div>`);
+}
 
 export const responsiveSizes = () => {
     return (`<div class="m-xxl">
