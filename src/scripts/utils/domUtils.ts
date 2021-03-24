7f284ef6f3de4b74c7970302dbd1795d2c66925f
Burton Smith <burton@breakstuff.io>
2021-03-23 20:08:20 -0400

    add additional DOM utils

--- src/scripts/utils/domUtils.ts
+++ src/scripts/utils/domUtils.ts
@@ -1,5 +1,7 @@
 declare global {
     interface HTMLElement {
+        find<T extends HTMLElement>(selector: string): T;
+        findAll<T extends HTMLElement>(selector: string): T[];
         on(event: string, callback: Function): void;
     }
 
@@ -19,6 +21,14 @@ export function $<T extends HTMLElement>(selector: string): T {
 export function $$<T extends HTMLElement>(selector: string): T[] {
     return Array.from(document.querySelectorAll(selector) as any) as T[];
 }
+
+HTMLElement.prototype.find = function <T extends HTMLElement>(selector: string): T {
+    return this.querySelector(selector) as T;
+};
+
+HTMLElement.prototype.findAll = function <T extends HTMLElement>(selector: string): T[] {
+    return Array.from(document.querySelectorAll(selector) as any) as T[];
+};
 
 HTMLElement.prototype.on = function (event: string, callback: Function) {
     this.addEventListener(event, callback);
