2b6c3e16bdae458e3c61153d2e31d957f012a033
Burton Smith <burton@breakstuff.io>
2021-12-18 22:45:27 -0500

    abbreviate display utility class names

--- src/scripts/components/utilities/display.stories.ts
+++ src/scripts/components/utilities/display.stories.ts
@@ -8,11 +8,11 @@ const alignments = ['', 'align-start', 'align-end', 'align-center', 'align-stret
 
 export const display = () => {
     return (`
-        <div class="bg-light p-lg m-md display-block">display-block</div>
-        <div class="bg-light p-lg m-md display-inline">display-inline</div>
-        <div class="bg-light p-lg m-md display-inline-block">display-inline-block</div>
-        <div class="bg-light p-lg m-md display-flex">display-flex</div>
-        <div class="bg-light p-lg m-md display-inline-flex">display-inline-flex</div>
+        <div class="bg-light p-lg m-md d-block">d-block</div>
+        <div class="bg-light p-lg m-md d-inline">d-inline</div>
+        <div class="bg-light p-lg m-md d-inline-block">d-inline-block</div>
+        <div class="bg-light p-lg m-md d-flex">d-flex</div>
+        <div class="bg-light p-lg m-md d-inline-flex">d-inline-flex</div>
     `);
 };
 
@@ -21,7 +21,7 @@ export const displayFlex = () => {
     const justify = select('justify', justifications, '');
     const align = select('align', alignments, '');
 
-    return (`<div style="min-height:10rem;" class="display-flex bg-info-lighter lg:hide md:show sm:display-flex ${direction} ${justify} ${align}">
+    return (`<div style="min-height:10rem;" class="d-flex bg-info-lighter lg:hide md:show sm:d-flex ${direction} ${justify} ${align}">
         <div class="bg-light b-xs p-sm">Column 1</div>
         <div class="bg-light b-xs p-sm">Column 2</div>
         <div class="bg-light b-xs p-sm">Column 3<br>Intentionally Weird</div>
