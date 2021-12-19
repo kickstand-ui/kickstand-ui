f63e08fc5dc62a8dacf764a5ea321a7c95105f15
Burton Smith <burton@breakstuff.io>
2021-12-18 23:08:20 -0500

    rename shadow util classes from `shadow` to `bs`

--- src/scripts/components/utilities/shadows.stories.ts
+++ src/scripts/components/utilities/shadows.stories.ts
@@ -4,14 +4,14 @@ const shadows = ['none', 'outline', 'inner', 'xs', 'sm', 'md', 'lg', 'xl'];
 
 export const shadowDisplay = () => {
     return (`<div>
-        <div class="m-xxl display-flex space-between">
-            ${shadows.map(s => `<div class="p-sm shadow-${s}">shadow-${s}</div>`).join('')}
+        <div class="m-xxl d-flex space-between">
+            ${shadows.map(s => `<div class="p-sm bs-${s}">bs-${s}</div>`).join('')}
         </div>
-        <div class="m-xxl display-flex space-between">
-            ${shadows.map(s => `<div class="p-sm hover:shadow-${s}">hover:shadow-${s}</div>`).join('')}
+        <div class="m-xxl d-flex space-between">
+            ${shadows.map(s => `<div class="p-sm hover:bs-${s}">hover:bs-${s}</div>`).join('')}
         </div>
-        <div class="m-xxl display-flex space-between">
-            ${shadows.map(s => `<div class="p-sm focus:shadow-${s}" tabindex="0">focus:shadow-${s}</div>`).join('')}
+        <div class="m-xxl d-flex space-between">
+            ${shadows.map(s => `<div class="p-sm focus:bs-${s}" tabindex="0">focus:bs-${s}</div>`).join('')}
         </div>
     </div>`);
 };
