b166624c3e9679e807dfb6cd36b4e85287e06e7f
Burton Smith <burton@breakstuff.io>
2022-01-01 17:03:59 -0500

    fix heading preprty in story

--- src/scripts/components/alert/alert.stories.ts
+++ src/scripts/components/alert/alert.stories.ts
@@ -6,7 +6,7 @@ const colors = ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 
 const dummyText = `“People will never be replaced by machines. In the end, life and business are about human connections. And computers are about trying to murder you in a lake. And to me the choice is easy.”<br><br>-- Michael Scott`;
 export const defaultHeading = () => {
     const color = select('color', colors, 'primary');
-    const header = text('header', 'The Facts of Life by Michael Scott');
+    const heading = text('heading', 'The Facts of Life by Michael Scott');
     const note = boolean('note', false);
     const dismissible = boolean('dismissible', false);
     const display = boolean('display', false);
@@ -14,7 +14,7 @@ export const defaultHeading = () => {
     const hideIcon = boolean('hide-icon', false);
     return (`
         <div class="m-xxl">
-            <ks-alert id="alert_test" icon="${icon}" hide-icon="${hideIcon}" header="${header}" note="${note}" color="${color}" dismissible="${dismissible}" display="${display}">${dummyText}</ks-alert>
+            <ks-alert id="alert_test" icon="${icon}" hide-icon="${hideIcon}" heading="${heading}" note="${note}" color="${color}" dismissible="${dismissible}" display="${display}">${dummyText}</ks-alert>
             <p>Some random text.</p>
             <div>
                 <ks-button shows="alert_test">Show Alert</ks-button>
