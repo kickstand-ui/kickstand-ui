e563283cc11463f480950fd921fd4e6195876480
Burton Smith <burton@breakstuff.io>
2022-02-14 17:35:17 -0500

    add manifest to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
 import "../src/scss/styles.scss";
 import { defineCustomElements } from "../dist/esm/loader";
+import { setCustomElementsManifest } from '@storybook/web-components';
+import customElements from '../custom-elements.json';
+
+setCustomElementsManifest(customElements);
 
 defineCustomElements();
